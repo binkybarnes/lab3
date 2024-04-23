041639894f75478ccf1f1f043b2a16f2c199c965
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-04-23 13:36:45 -0700

    fixed ArrayExamples reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,31 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void ReverseInPlaceTest2() {
+    int[] arr = { 1, 2 };
+    ArrayExamples.reverseInPlace(arr);
+    assertArrayEquals(new int[] { 2, 1 }, arr);
+  }
+
+  @Test
+  public void ReversedTest2() {
+    int[] arr = { 1, 2 };
+    assertArrayEquals(new int[] { 2, 1 }, ArrayExamples.reversed(arr));
+    assertArrayEquals(new int[] { 1, 2 }, arr);
+
   }
 }
